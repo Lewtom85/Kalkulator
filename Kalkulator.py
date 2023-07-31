29f487fbbd389bfe63b3e04fd2d208e8bacc6666
Tomasz Lewandowski <lewtom85@gmail.com>
2023-07-31 10:12:07 +0200

    Add more function

--- Kalkulator.py
+++ Kalkulator.py
@@ -1,15 +1,16 @@
-#Kalkulator
-
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
-print("Podaj dzialanie, posugujc sie odpowiednia liczba: \n 1 Dodawanie \n 2 Odejmowanie \n 3 Mnozenie \n 4 Dzielenie")
-operation = int(input("Wskaz numer wybranego dzialania "))
 
-print("Podaj pierwsza liczbe dzialania: ")
-x = float(input("Podaj swoja pierwsza liczbe: "))
-print("Podaj druga liczbe dzialania: ")
-y = float(input("Podaj swoja druga liczbe: "))
+def dates():
+    print("Podaj dzialanie, posugujc sie odpowiednia liczba: \n 1 Dodawanie \n 2 Odejmowanie \n 3 Mnozenie \n 4 Dzielenie")
+    operation = int(input("Wskaz numer wybranego dzialania "))
+
+    print("Podaj pierwsza liczbe dzialania: ")
+    x = float(input("Podaj swoja pierwsza liczbe: "))
+    print("Podaj druga liczbe dzialania: ")
+    y = float(input("Podaj swoja druga liczbe: "))
+    return operation, x, y
 
 def add(x, y):
     logging.info(f"Dodawanie {x} + {y} rowna sie: ")
@@ -25,16 +26,19 @@ def multiply(x, y):
 
 def divide(x, y):
     logging.info(f"Dzielenie {x} / {y} rowna sie: ")
-    return x/y
+    return x / y
 
-if operation == 1:
-    print(add(x, y))
+def main():
+    operation, x, y = dates()
 
-elif operation == 2:
-    print(subtract(x, y))
+    if operation == 1:
+        print(add(x, y))
+    elif operation == 2:
+        print(subtract(x, y))
+    elif operation == 3:
+        print(multiply(x, y))
+    elif operation == 4:
+        print(divide(x, y))
 
-elif operation == 3:
-    print(multiply(x, y))
-
-elif operation == 4:
-    print(divide(x, y))
+if __name__ == "__main__":
+    main()
